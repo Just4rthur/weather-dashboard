3e2867244fc17a07f06ad2aa91b52550a238222c
Arturo Bendezu <artycok123@gmail.com>
2025-07-03 22:10:28 +0200

    feat: Added map view recenter every time a new city is prompted

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,4 +1,18 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import { useEffect } from "react";
+
+function ChangeMapView({ lat, lon }) {
+  const map = useMap();
+
+  useEffect(() => {
+    map.flyTo([lat, lon], map.getZoom(), {
+      animate: true,
+      duration: 1.5,
+    });
+  }, [lat, lon, map]);
+
+  return null;
+}
 
 const MapView = ({ lat, lon }) => (
   <MapContainer
@@ -7,10 +21,13 @@ const MapView = ({ lat, lon }) => (
     scrollWheelZoom={true}
     style={{ height: "400px", width: "100%" }}
   >
+    <ChangeMapView lat={lat} lon={lon} />
+
     <TileLayer
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       attribution="© OpenStreetMap contributors"
     />
+
     <Marker position={[lat, lon]}>
       <Popup>Selected Location</Popup>
     </Marker>
